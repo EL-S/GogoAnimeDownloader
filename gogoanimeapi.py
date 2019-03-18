f92ab3de3608be320cf12c0eeca5f447a348b4bf
EL-S <31227083+EL-S@users.noreply.github.com>
2019-03-18 23:49:22 +1100

    General Changes
    Various general changes to the code

--- gogoanimeapi.py
+++ gogoanimeapi.py
@@ -17,7 +17,7 @@ ep_end = str(5001)
 default_ep = str(1)
 #quality_preferred = str(360) #1080p,720p,480p,360p
 #quality = "lowest" #highest,lowest,average
-
+http_client = ""
 start_anime_id = 0
 end_anime_id = 10000
 
@@ -68,6 +68,7 @@ def get_m3u8_initiator_src(video_src):
     return m3u8_src
 
 def get_m3u8_playlist_src(m3u8_src,quality_preferred,quality,headers):
+    quality = quality.lower()
     req = requests.get(m3u8_src, headers=headers) #can sometimes fail to load, too many connections in short time maybe
     m3u8_stream = req.text
     m3u8_stream_array = m3u8_stream.split("\n")
@@ -140,12 +141,14 @@ def save_playlist_information(m3u8_src,directory,anime_name,m3u8_quality_playlis
 
 def download_ts_files(m3u8_links,url_domain,headers,path,threads):
     try:
-        global i
+        global i, http_client
         i = 0
         http_client = httpclient.AsyncHTTPClient(force_instance=True,defaults=dict(user_agent="Mozilla/5.0"),max_clients=threads)
         for sub_link in m3u8_links:
             url = url_domain+sub_link
-            request = httpclient.HTTPRequest(url.strip(),headers=headers,method='GET',connect_timeout=10000,request_timeout=10000)
+            wait_time1 = (i/8) + 90
+            wait_time2 = (i/8) + 150
+            request = httpclient.HTTPRequest(url.strip(),headers=headers,method='GET',connect_timeout=wait_time1,request_timeout=wait_time2)
             http_client.fetch(request,handle_ts_file_response)
             i += 1
         ioloop.IOLoop.instance().start()
@@ -155,27 +158,41 @@ def download_ts_files(m3u8_links,url_domain,headers,path,threads):
         
 
 def handle_ts_file_response(response):
-    global i,path,headers
+    global i,path,headers, http_client
     if response.code == 599:
-        print(response.effective_url,"error")
-        http_client.fetch(response.effective_url.strip(),headers=headers,method='GET',connect_timeout=10000,request_timeout=10000)
+        #http_client.fetch(response.effective_url.strip(),headers=headers,method='GET',connect_timeout=10000,request_timeout=10000)
+        #request = httpclient.HTTPRequest(response.effective_url.strip(),headers=headers,method='GET',connect_timeout=10000,request_timeout=10000)
+        #http_client.fetch(request,handle_ts_file_response)
+        try:
+            wait_time1 = (i/8) + 20
+            wait_time2 = (i/8) + 60
+            request = httpclient.HTTPRequest(response.effective_url,headers=headers,method='GET',connect_timeout=wait_time1,request_timeout=wait_time2)
+            http_client.fetch(request,handle_ts_file_response)
+        except Exception as e:
+            pass
+        else:
+            file_name = str(response.effective_url.split("/")[-1:][0])
+            if not silent_setting:
+                print("Retrying:",file_name)
     else:
-        global i,path
         try:
             file_name = str(response.effective_url.split("/")[-1:][0])
             m3u8_video_file_part = response.body #use binary
             with open(path+file_name, "wb") as file: #save the file as a binary
                 file.write(m3u8_video_file_part)
             if not silent_setting:
-                print("Downloaded:",file_name)
+                print("Downloaded ({} remaining):".format(i),file_name)
             #print("alive",response.effective_url)
+            i -= 1
         except Exception as e:
-            print("dead",response.effective_url,e)
-            http_client.fetch(response.effective_url.strip(),headers=headers,method='GET',connect_timeout=10000,request_timeout=10000)
-        i -= 1
-        if i == 0: #all pages loaded
-            ioloop.IOLoop.instance().stop()
-            #print("Download Complete")
+            pass
+            #print("dead",response.effective_url,e)
+            #http_client.fetch(response.effective_url.strip(),headers=headers,method='GET',connect_timeout=10000,request_timeout=10000)
+            #request = httpclient.HTTPRequest(response.effective_url.strip(),headers=headers,method='GET',connect_timeout=10000,request_timeout=10000)
+            #http_client.fetch(request,handle_ts_file_response)
+    if i == 0: #all pages loaded
+        ioloop.IOLoop.instance().stop()
+        #print("Download Complete")
 
 def download_episode(url,directory="Episodes/",convert=True,output_format=".mkv",overwrite=True,keep_source_stream=False,silent=True,quality_preferred="default",quality="highest",threads="100",headers={"Origin": "https://vidstreaming.io", "Referer": "https://vidstreaming.io"}):
     global silent_setting
